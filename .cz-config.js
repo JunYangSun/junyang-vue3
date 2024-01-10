5777fe88b8111faa20c42dad03610eb0633067cd
JunYangSun <sunjunyang2023@163.com>
2024-01-10 14:41:49 +0800

    feat(git): add commitizen
    使用commitizen规范代码提交

---
+++ .cz-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // 可选类型
+  types: [
+    { value: 'feat', name: 'feact:  新功能' },
+    { value: 'fix', name: 'fix:   修复' },
+    { value: 'docs', name: 'docs:   文档变更' },
+    { value: 'style', name: 'style:   代码格式(不影响代码运行的变动)' },
+    {
+      value: 'refactor',
+      name: 'refactor:   重构(既不是增加feature,也不是修复bug'
+    },
+    { value: 'perf', name: 'perf:   性能优化' },
+    { value: 'test', name: 'test:   增加测试' },
+    { value: 'chore', name: 'chore:   构建过程或辅助工具的变动' },
+    { value: 'revert', name: 'revert:   回退' },
+    { value: 'build', name: 'build:   打包' }
+  ],
+  // 步骤
+  message: {
+    type: '请选择提交的类型:',
+    customScope: '请输入修改的范围(可选)',
+    subject: '请教要描述提交(必填)',
+    body: '请输入详细描述(可选)',
+    footer: '请输入要关闭的issue(可选)',
+    confirmCommit: '确认要使用以上信息提交(y/n)'
+  },
+  // 默认长度为72
+  subjectLimit: 72
+}
